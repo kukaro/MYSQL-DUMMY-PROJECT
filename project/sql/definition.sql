39c664f13e54d16e07059eb143848d81e0b55ddd
kukaro <justkukaro@naver.com>
2020-08-29 21:23:46 +0900

    더미 추가

---
+++ project/sql/definition.sql
@@ -0,0 +1,5 @@
+/**
+INSERT INTO user(email, name, age, birth_date, password, created_date, updated_date)
+    VALUES('dydy@dydy.dy7', '동길', 53, '2008-01-04 00:00:01', '4567', '2008-01-01 00:00:01', '2008-01-01 00:00:01');
+
+ */
